c655dba1de329de94a9567888783671ce29a913e
Ambesh <ambeshgaunker@gmail.com>
2024-06-27 18:05:32 +0530

    adding mocking for database

--- Testing/simple-express-vitest/src/test/index.test.ts
+++ Testing/simple-express-vitest/src/test/index.test.ts
@@ -1,6 +1,8 @@
-import { describe, expect, test, it } from "vitest";
+import { describe, expect, test, it, vi } from "vitest";
 import request from "supertest";
 import { app } from "../index";
+
+vi.mock("../db");
 
 describe("POST /sum", () => {
   it("should return the sum of two numbers", async () => {
